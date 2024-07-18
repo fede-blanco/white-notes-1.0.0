d8315b38e0d7bb5e00886067a7fc4de42eea4a0d
fede-blanco <blancofedem@gmail.com>
2024-07-18 11:26:07 -0300

    signout redirect to home fix

--- src/components/buttons/LogoutBoutton.tsx
+++ src/components/buttons/LogoutBoutton.tsx
@@ -3,11 +3,18 @@
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { signOut } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 export default function LogoutButton() {
+
+  const router = useRouter()
+
   return (
     <button
-      onClick={() => signOut()}
+      onClick={() => {
+        signOut()
+        router.push("/")
+      }}
       className="btn btn-red ml-2 inline-flex items-center gap-1"
     >
       Logout <FontAwesomeIcon className="h-5" icon={faArrowRightFromBracket} />
